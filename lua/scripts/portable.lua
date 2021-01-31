7e1acb89eb7fa03f05aa8e3a0a81d02eafc69433
Sudospective <avery@sudospective.net>
2021-01-31 13:29:01 -0600

    Added parts of wubwubwubwub and speen

--- lua/scripts/portable.lua
+++ lua/scripts/portable.lua
@@ -1,3 +1,33 @@
+--If you plan on using this:
+--[[
+Required manual edits: (@ is axis, # is column in {1, 2, 3, 4}
+- confusion@offset# -> targetrotation@#
+- move@# -> target@#
+- sw, sh -> bw, bh (if you are inferring SD theme scaling)
+
+In-progress conversions:
+- noteskewx# -> targetskewx#
+]]
+
+bw, bh = 640, 480
+sm_scale = sh / bh
+sm_fix = (FUCK_EXE and 0) or 1
+
+--[[
+	////////////////////////////////////////////////////////////////
+	// USE THESE COMPATIBLITY MODS INSTEAD OF THE NITG EQUIVALENT //
+	////////////////////////////////////////////////////////////////
+
+	- confusion_offset#: targetrotation_# (# starts at 1 instead of 0)
+	- SetHiddenRegion on P[pn]: 'hiddenregion' in set (mod number is length of region)
+	- Anything using screen variables (sw, sh) should use the defined base variables (bw, bh)
+
+	Everything else should work the same.
+
+	TODO:
+	- Column-specific noteskew conversion
+--]]
+
 -- Generic Conversion
 alias
 {'targetrotationx1', 'confusionxoffset'..sm_fix}			-- Column ConfusionX
@@ -15,57 +45,116 @@ alias
 {'targetrotationz3', 'confusionoffset'..sm_fix + 2}
 {'targetrotationz4', 'confusionoffset'..sm_fix + 3}
 
+{'targetx1', 'movex'..sm_fix}									-- Column MoveX
+{'targetx2', 'movex'..sm_fix + 1}
+{'targetx3', 'movex'..sm_fix + 2}
+{'targetx4', 'movex'..sm_fix + 3}
+
+{'targety1', 'movey'..sm_fix}									-- Column MoveY
+{'targety2', 'movey'..sm_fix + 1}
+{'targety3', 'movey'..sm_fix + 2}
+{'targety4', 'movey'..sm_fix + 3}
+
+{'targetz1', 'movez'..sm_fix}									-- Column MoveZ
+{'targetz2', 'movez'..sm_fix + 1}
+{'targetz3', 'movez'..sm_fix + 2}
+{'targetz4', 'movez'..sm_fix + 3}
+
+{'targetskewx1', 'noteskewx'..sm_fix}									-- Column NoteSkewX
+{'targetskewx2', 'noteskewx'..sm_fix + 1}
+{'targetskewx3', 'noteskewx'..sm_fix + 2}
+{'targetskewx4', 'noteskewx'..sm_fix + 3}
+
 {'confusionzoffset', 'confusionoffset'}							-- no i like the z in there
+
+function hideregion(t)
+	local pn = t.plr or get_plr()
+	if type(pn) == 'number' then
+		if FUCK_EXE then
+			P[pn]:SetHiddenRegions({{t[1], t[2]}})
+		else
+			P[pn]:GetChild('NoteField'):add_hidden_regions({{t[1], t[2]}})
+		end
+	elseif type(pn) == 'table' then
+
+		for _, plr in ipairs(plr) do
+			if FUCK_EXE then
+				P[pn]:SetHiddenRegions({{t[1], t[2]}})
+			else
+				P[pn]:GetChild('NoteField'):add_hidden_regions({{t[1], t[2]}})
+			end
+		end
+	end
+	return hideregion
+end
+
 
 -- SM Specific Conversion
 if not FUCK_EXE then
+	--local Actor = ...
+	Actor:fardistz(1000 * sm_scale)
 	aux
 	{'x', 'y', 'z'}													-- X, Y, and Z position tweenmods
 	{'rotationx', 'rotationy', 'rotationz'}							-- X, Y, and Z rotation tweenmods
-    {'tiny'}														-- Non-column to column tiny
+	{'tiny'}														-- Non-column to column tiny
+	{'hide'}														-- cmon man
+	{'hidenoteflash'}												-- THANKS SM5 VERY COOL
 
 	node
 	{
-	    'x',
-	    function(x)
-			P[get_plr()]:GetChild('NoteField'):x(x)
-	    end
+		'x',
+		function(n, pn)
+			if P[pn] then P[pn]:GetChild('NoteField'):x(n) end
+		end
 	}
 	{
-	    'y',
-	    function(y)
-			P[get_plr()]:GetChild('NoteField'):y(y)
-	    end
+		'y',
+		function(n, pn)
+			if P[pn] then P[pn]:GetChild('NoteField'):y(n) end
+		end
 	}
 	{
-	    'z',
-	    function(z)
-			P[get_plr()]:GetChild('NoteField'):z(z)
-	    end
+		'z',
+		function(n, pn)
+			if P[pn] then P[pn]:GetChild('NoteField'):z(n) end
+		end
 	}
 	{
-	    'rotationx',
-	    function(rotationx)
-	        P[get_plr()]:GetChild('NoteField'):rotationx(rotationx)
-	    end
+		'rotationx',
+		function(n, pn)
+			if P[pn] then P[pn]:GetChild('NoteField'):rotationx(n) end
+		end
 	}
 	{
-	    'rotationy',
-	    function(rotationy)
-	        P[get_plr()]:GetChild('NoteField'):rotationy(rotationy)
-	    end
+		'rotationy',
+		function(n, pn)
+			if P[pn] then P[pn]:GetChild('NoteField'):rotationy(n) end
+		end
 	}
 	{
-	    'rotationz',
-	    function(rotationz)
-	        P[get_plr()]:GetChild('NoteField'):rotationz(rotationz)
-	    end
+		'rotationz',
+		function(n, pn)
+			if P[pn] then P[pn]:GetChild('NoteField'):rotationz(n) end
+		end
 	}
-    {
-        'tiny',
-        function(tiny)
-            return tiny
-        end,
-        'tiny1', 'tiny2', 'tiny3', 'tiny4'
-    }
+	{
+		'tiny',
+		function(n)
+			return n, n
+		end,
+		'tinyx', 'tinyy'
+	}
+	{
+		'hide',
+		function(n, pn)
+			if P[pn] then P[pn]:GetChild('NoteField'):visible(n <= 0) end
+		end
+	}
+	{
+		'hidenoteflash',
+		function(n)
+			return n, n, n, n
+		end,
+		'hidenoteflash1', 'hidenoteflash2', 'hidenoteflash3', 'hidenoteflash4'
+	}
 end
--- lua/sudo.lua
+++ lua/sudo.lua
@@ -1,1 +1,47 @@
-set {1, 1.75, 'xmod'} -- like this
+-- squinshies
+func {136, function()
+    for pn = 1, 2 do
+        P[pn]:x(scx)
+    end
+end}
+mirror {142, 2, inExpo, 50, 'drunk', 50, 'tipsy'}
+for beat = 144, 190, 2 do
+    mirror
+    {beat - 0.5, 1, inOutQuad, 100, 'tinyy', -100, 'tinyx'}
+    {beat + 0.5, 1, inOutQuad, -100, 'tinyy', 100, 'tinyx'}
+    ease
+    {beat - 0.5, 1, inOutQuad, 5000, 'tinyz', plr = 1}
+    {beat - 0.5, 1, inOutQuad, -2500, 'tinyz', plr = 2}
+    {beat + 0.5, 1, inOutQuad, -2500, 'tinyz', plr = 1}
+    {beat + 0.5, 1, inOutQuad, 5000, 'tinyz', plr = 2}
+end
+ease {191.5, 1, inOutQuad, 0, 'tinyz', 0, 'tinyy', 0, 'tinyx'}
+ease {192, 2, outExpo, 0, 'drunk', 0, 'tipsy'}
+
+-- WUBWUBWUBWUB
+add 
+{208, 2, pop, 1000, 'zoomx'}
+{210, 5, spike, -20000, 'tinyz'}
+{211, 3, inverse, 100, 'invert', 200, 'bumpy', 90, 'rotationy'}
+mirror
+{214, 0.5, inOutQuad, 30, 'tinyy', -30, 'tinyx', -100, 'drunk', 100, 'tipsy'}
+{214.5, 0.5, inOutQuad, -30, 'tinyy', 30, 'tinyx', 100, 'drunk', -100, 'tipsy'}
+{215, 0.5, inOutQuad, 30, 'tinyy', -30, 'tinyx', -100, 'drunk', 100, 'tipsy'}
+{215.5, 0.5, inOutQuad, -30, 'tinyy', 30, 'tinyx', 100, 'drunk', -100, 'tipsy'}
+{216, 0.5, inOutQuad, 0, 'tinyy', 0, 'tinyx', 0, 'drunk', 0, 'tipsy'}
+ease
+{219.5, 0.5, outExpo, 100, 'invert'}
+{220, 0.5, outExpo, 100, 'reverse'}
+{220.6, 0.5, outExpo, 0, 'invert', 100, 'flip'}
+{221, 0.5, outExpo, 0, 'reverse'}
+{221.5, 0.5, outExpo, 0, 'flip'}
+add
+{226, 5, spike, -20000, 'tinyz'}
+{227, 3, inverse, 100, 'invert', 200, 'bumpy', -90, 'rotationy'}
+
+-- S P E E N
+add
+{256, 12, inQuad, 15, 'rotationz', 360 * 8, 'rotationy', -(314.15 * 16), 'confusionyoffset', -100, 'flip', 200, 'tinyy'}
+{260, 8, inExpo, -2000, 'tinyz', 50, 'reverse'}
+set {268, 0, 'rotationz', 0, 'rotationy', 0, 'tinyy', 0, 'tinyz', 0, 'reverse', 2000, 'z'}
+ease {268, 2, outExpo, 0, 'z', 0, 'flip'}
