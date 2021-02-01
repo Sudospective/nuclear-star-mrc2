d0557849cbf824a63dcf023359c63d30e657f25e
Sudospective <avery@sudospective.net>
2021-02-01 15:01:58 -0600

    wow another push this is crazy haha

--- lua/sudo.lua
+++ lua/sudo.lua
@@ -39,13 +39,13 @@ set
 ease
     {136, 1.5, outExpo, 100, 'dark', 100, 'invert', -314.15, 'confusionzoffset'}
     {136, 1, outExpo, 0, 'drunk', 0, 'tipsy', 0, 'flip', 0.15, 'xmod', 0, 'rotationx', -314.15 / 4, 'confusionxoffset', -10000, 'tinyz', 0, 'tiny', 0, 'longholds'}
-    {136, 6, inOutQuad, 0.5, 'xmod', 0, 'rotationx', 0, 'confusionxoffset', 100, 'zoomy', 0, 'tinyz', 0, 'stealth', 0, 'confusionzoffset'}
+    {136, 7, inOutQuad, 0.5, 'xmod', 0, 'rotationx', 0, 'confusionxoffset', 100, 'zoomy', 0, 'tinyz', 0, 'stealth', 0, 'confusionzoffset'}
     {142, 2, spike, 50, 'stealth'}
     {142.5, 1, inOutExpo, 0, 'stealth2', 0, 'stealth3', plr = 1}
     {142.5, 1, inOutExpo, 0, 'stealth0', 0, 'stealth1', plr = 2}
     {142, 2, inExpo, 1.5, 'xmod', 100, 'wave', 15, 'tornado', -150, 'holdgirth', 200, 'bumpyperiod', 100, 'bumpyxperiod', 100, 'bumpyxoffset', 50, 'drunk', 50, 'tipsy'}
     {143, 1, inOutExpo, 100, 'dark', 50, 'beat',  0, 'holdstealth'}
-    {143, 0.5, outExpo, 0, 'invert'}
+    {142.5, 1, outBack, 0, 'invert'}
 mirror
     {136, 6, outBack, 10, 'rotationy', -314.15 / 18, 'confusionyoffset'}
     {140, 4, inExpo, 0, 'rotationy', 0, 'confusionyoffset'}
@@ -66,48 +66,57 @@ for beat = 144, 190, 2 do
         {beat, 1, bounce, 0, 'wave'}
         {beat + 1, 1, bounce, 0, 'wave'}
 end
+
+set {175, 0, 'dark', 100, 'dark0', 100, 'dark1', 100, 'dark2', 100, 'dark3'}
 ease
     {175.5, 2, bell, 0, 'noteskewx', 0, 'noteskewy', 0, 'tinyx', 0, 'tinyy', 0, 'tinyz', 0, 'beat', 0, 'drunk', 0, 'tipsy', 0, 'wave', 0, 'tornado', 0, 'bumpy', 0, 'bumpyx'}
     {176, 1, bounce, 20, 'flip', 70, 'dark'}
-    {177, 1, outExpo, -25, 'flip', -25, 'invert'}
+    {177, 1, outExpo, -50, 'flip', -50, 'invert'}
+    {176, 1, inBack, -25, 'cross', 100, 'movey1', 100, 'movey2', 1.75, 'xmod'}
+    {177, 1, outSine, 0, 'dark1', 0, 'dark2', plr = 1}
 swap
-    {177, 0.5, outExpo, 'ludr'}
-    {178, 0.5, outExpo, 'ldur'}
-    {178.5, 0.5, outExpo, 'ludr'}
-    {179, 0.5, outExpo, 'ldur'}
-    {180, 0.5, outExpo, 'ludr'}
-    {181, 0.5, outExpo, 'ldur'}
-    {181.5, 0.5, outExpo, 'ludr'}
-    {182.5, 0.5, outExpo, 'ldur'}
-    {183, 0.5, outExpo, 'ludr'}
-    {183.5, 0.5, outExpo, 'ldur'}
-    {184, 0.5, outExpo, 'ludr'}
-    {185, 0.5, outExpo, 'ldur'}
-    {186, 0.5, outExpo, 'ludr'}
-    {186.5, 0.5, outExpo, 'ldur'}
-    {187, 0.5, outExpo, 'ludr'}
-    {188, 0.5, outExpo, 'ldur'}
-    {188.5, 0.5, outExpo, 'ludr'}
-    {189, 0.5, outExpo, 'ldur'}
-
-set {190, 0, 'dark', 100, 'dark0', 100, 'dark1', 100, 'dark2', 100, 'dark3'}
+    {177, 0.5, outQuad, 'ludr'}
+    {178, 0.5, outQuad, 'ldur'}
+    {178.5, 0.5, outQuad, 'ludr'}
+    {179, 0.5, outQuad, 'ldur'}
+    {180, 0.5, outQuad, 'ludr'}
+    {181, 0.5, outQuad, 'ldur'}
+    {181.5, 0.5, outQuad, 'ludr'}
+    {182.5, 0.5, outQuad, 'ldur'}
+    {183, 0.5, outQuad, 'ludr'}
+    {183.5, 0.5, outQuad, 'ldur'}
+    {184, 0.5, outQuad, 'ludr'}
+    {185, 0.5, outQuad, 'ldur'}
+    {186, 0.5, outQuad, 'ludr'}
+    {186.5, 0.5, outQuad, 'ldur'}
+    {187, 0.5, outQuad, 'ludr'}
+    {188, 0.5, outQuad, 'ldur'}
+    {188.5, 0.5, outQuad, 'ludr'}
+    {189, 0.5, outQuad, 'ldur'}
 ease
+    {177, 1, linear, 0, 'cross', 0, 'movey1', 0, 'movey2', 1.5, 'xmod'}
+    {189, 1, linear, 100, 'dark1', 100, 'dark2', plr = 1}
+    {189, 1, inBack, 0, 'flip', 0, 'invert'}
     {190, 0.5, outBack, 100, 'reverse0', 0, 'dark0'}
     {190.25, 0.5, outBack, 100, 'reverse1', 0, 'dark1'}
     {190.5, 0.5, outBack, 100, 'reverse2', 0, 'dark2'}
     {190.75, 0.5, outBack, 100, 'reverse3', 0, 'dark3'}
 
     {191, 1, outBack, 0, 'reverse0', 0, 'reverse1', 0, 'reverse2', 0, 'reverse3'}
-    {191, 1, inExpo, 0, 'flip', 0, 'invert'}
     {191, 1, inOutSine, 0, 'wave', 0, 'tornado', 0, 'tinyz', 0, 'tinyy', 0, 'tinyx', 0, 'holdgirth', 0, 'bumpy', 0, 'bumpyx', 0, 'drunk', 0, 'tipsy', 0, 'dark', 0, 'beat'}
 
 -- A DRIVEN DROP???? BY SUDO??????????
 set {192, 150, 'movey'}
 ease {192, 8, linear, 1200, 'centered2'}
 for beat = 192, 199 do
-    mirroradd {beat, 1, bounce, -350, 'movez'}
-    add {beat, 1, bounce, -20, 'flip'}
-    add {beat, 0, instant, -150, 'movey'}
+    mirroradd
+        {beat, 1, bounce, -350, 'movez'}
+        {beat, 0.5, pop, 150, 'drunk', 150, 'tipsy'}
+    add
+        {beat, 1, bounce, -20, 'flip'}
+        {beat, 0, instant, -150, 'movey'}
+        {beat, 1, pop, 100, 'stealth', plr = 1}
+        {beat, 1, pop, 20, 'stealth', plr = 2}
 end
 swap
     {192, 0.5, outExpo, 'dlru'}
@@ -122,7 +131,6 @@ ease {200, 6, outQuad, 0, 'movey', 0, 'centered2'}
 
 
 -- WUBWUBWUBWUB
-set {206, 200, 'tornadoperiod'}
 swap
     {206.5, 0.5, outExpo, 'dlru'}
     {207, 0.5, outExpo, 'urld'}
@@ -132,6 +140,7 @@ add
     {206, 4, inverse, -100, 'zoomx', 45, 'rotationx', 10000, 'parabolaz', 50, 'reverse', 5000, 'tinyz', 800, 'zoomz', 100, 'stealth'}
     {210, 5, spike, 40000, 'tinyz', 1000, 'longholds', 100, 'tornado'}
     {211.5, 2, bounce, 95, 'stealth'}
+    {211, 3, bell, 800, 'tornadoperiod'}
     {211, 3, inverse, 10, 'rotationx', -314.15 / 18, 'confusionxoffset', 100, 'invert', 100, 'bumpy', -100, 'reverse'}
     {211, 3, spike, 45, 'rotationy', -314.15 / 4, 'confusionyoffset', -800, 'zoomz'}
     {216, 1, outExpo, 400, 'zoomz'}
@@ -161,13 +170,19 @@ add
     {222, 2, outExpo, 360, 'rotationz', -314.15 * 2, 'confusionzoffset'}
     {226, 5, spike, 40000, 'tinyz', 1000, 'longholds', 100, 'tornado'}
     {227.5, 2, bounce, 95, 'stealth'}
+    {227, 3, bell, 800, 'tornadoperiod'}
     {227, 3, inverse, 10, 'rotationx', -314.15 / 18, 'confusionxoffset', 100, 'invert', 100, 'bumpy', -100, 'reverse'}
     {227, 3, spike, -45, 'rotationy', 314.15 / 4, 'confusionyoffset', -800, 'zoomz'}
 
+--[[ --goddammit shut up sudo
+ease
+    {234, 4, bounce, -400, 'bumpy', 400, 'bumpyperiod'}
+]]--
 add 
     --{238, 4, inverse, -100, 'zoomx', -45, 'rotationx', -10000, 'parabolaz', 50, 'reverse', 5000, 'tinyz'} -- maybe something else a second time
     {242, 5, spike, 40000, 'tinyz', 1000, 'longholds', 100, 'tornado'}
     {243.5, 2, bounce, 95, 'stealth'}
+    {243, 3, bell, 800, 'tornadoperiod'}
     {243, 3, inverse, 10, 'rotationx', -314.15 / 18, 'confusionxoffset', 100, 'invert', 100, 'bumpy', -100, 'reverse'}
     {243, 3, spike, 45, 'rotationy', -314.15 / 4, 'confusionyoffset', -800, 'zoomz'}
 mirror
@@ -182,7 +197,6 @@ ease
     {252.5, 0.5, outExpo, 0, 'invert', 100, 'flip', 10, 'rotationy'}
     {253, 0.5, outExpo, 0, 'reverse'}
     {253.5, 0.5, outExpo, 0, 'flip', 0, 'rotationy', 100, 'zoomz'}
-set {256, 100, 'tornadoperiod'}
 
 -- actually tornado
 for beat = 256, 266, 2 do
